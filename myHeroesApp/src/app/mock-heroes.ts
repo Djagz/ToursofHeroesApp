b98e3584b1e8ed43750e1b956f44be21de56ac84
Djagz <jagwanidipesh85@gmail.com>
2018-01-06 16:14:12 -0800

    Completed Service and Routing Of Components

---
+++ myHeroesApp/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+
+{ id: 1, name: "Captain America" },
+{ id: 2, name: "Iron Man" },
+{ id: 3, name: "Thor" },
+{ id: 4, name: "Wonder Woman" },
+{ id: 5, name: "Super Man" },
+{ id: 6, name: "Hulk" },
+{ id: 7, name: "Spider Man" },
+{ id: 8, name: "Flash" }
+
+]
